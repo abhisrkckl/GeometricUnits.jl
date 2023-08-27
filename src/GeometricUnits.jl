34e6e328f36bb9f9d0b5c7e81995a077fe0e5608
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-08-27 14:34:31 -0500

    == approx * /

--- src/GeometricUnits.jl
+++ src/GeometricUnits.jl
@@ -1,12 +1,23 @@
 module GeometricUnits
 
-import Base.+, Base.-, Base.time, Base.zero
+import Base.+, Base.-, Base.*, Base./, Base.==, Base.isapprox, Base.time, Base.zero
 
-export GQ, dimension_mismatch, dimensionless, angle, speed, time, distance, frequency, acceleration, unit, zero, quantity
+export GQ,
+    dimension_mismatch,
+    dimensionless,
+    angle,
+    speed,
+    time,
+    distance,
+    frequency,
+    acceleration,
+    unit,
+    zero,
+    quantity
 
 dimension_mismatch(obj) = throw(DomainError(obj, "Dimension mismatch!"))
 
-struct GQ{X<:Real}
+struct GQ{X<:AbstractFloat}
     x::X
     d::Int
 end
@@ -15,20 +26,38 @@ unit(a::GQ) = GQ(oneunit(a.x), a.d)
 zero(a::GQ) = GQ(zero(a.x), a.d)
 quantity(a::GQ, y) = GQ(oftype(a.x, y), a.d)
 
-dimensionless(x::Float64) = GQ{Float64}(x, 0)
-angle(x::Float64) = GQ{Float64}(x, 0)
-speed(x::Float64) = GQ{Float64}(x, 0)
+dimensionless(x) = GQ(x, 0)
+angle(x) = GQ(x, 0)
+speed(x) = GQ(x, 0)
+time(x) = GQ(x, 1)
+distance(x) = GQ(x, 1)
+frequency(x) = GQ(x, -1)
+acceleration(x) = GQ(x, -1)
 
-time(x::Float64) = GQ{Float64}(x, 1)
-distance(x::Float64) = GQ{Float64}(x, 1)
+a::GQ == b::GQ = (a.d == b.d) ? (a.x == b.x) : dimension_mismatch((a, b))
+a::GQ == y::Real = (a.d == 0) ? (a.x == y) : dimension_mismatch((a, y))
+x::Real == b::GQ = b == a
 
-frequency(x::Float64) = GQ{Float64}(x, -1)
-acceleration(x::Float64) = GQ{Float64}(x, -1)
+isapprox(a::GQ, b::GQ) = (a.d == b.d) ? isapprox(a.x, b.x) : dimension_mismatch((a, b))
+isapprox(a::GQ, y::Real) = (a.d == 0) ? isapprox(a.x, y) : dimension_mismatch((a, y))
+isapprox(x::Real, b::GQ) = isapprox(b, x)
 
 a::GQ + b::GQ = (a.d == b.d) ? GQ(a.x + b.x, a.d) : dimension_mismatch((a, b))
+a::GQ + y::Real = (a.d == 0) ? GQ(a.x + y, a.d) : dimension_mismatch((a, y))
+x::Real + b::GQ = b + x
 
 a::GQ - b::GQ = (a.d == b.d) ? GQ(a.x - b.x, a.d) : dimension_mismatch((a, b))
+a::GQ - y::Real = (a.d == 0) ? GQ(a.x - y, a.d) : dimension_mismatch((a, y))
+x::Real - b::GQ = (b.d == 0) ? GQ(x - b.x, b.d) : dimension_mismatch((x, b))
 
 -a::GQ = GQ(-a.x, a.d)
+
+a::GQ * b::GQ = GQ(a.x * b.x, a.d + b.d)
+a::GQ * y::Real = GQ(a.x * y, a.d)
+x::Real * b::GQ = b * x
+
+a::GQ / b::GQ = GQ(a.x / b.x, a.d - b.d)
+a::GQ / y::Real = GQ(a.x / y, a.d)
+x::Real / b::GQ = GQ(x / b.x, -b.d)
 
 end # module GeometricUnits
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,22 @@ using Test
 
     f1 = frequency(0.2)
 
+    d1 = dimensionless(6.0)
+
     @testset "unit and zero" begin
         @test unit(t1).x == 1 && unit(t1).d == t1.d
         @test zero(t1).x == 0 && zero(t1).d == t1.d
 
         @test unit(f1).x == 1 && unit(f1).d == f1.d
         @test zero(f1).x == 0 && zero(f1).d == f1.d
+    end
+
+    @testset "comparison" begin
+        @test t1 == t1
+        @test_throws DomainError t1 == f1
+
+        @test t1 ≈ t1
+        @test_throws DomainError t1 ≈ f1
     end
 
     @testset "addition and subtraction" begin
@@ -24,5 +34,37 @@ using Test
 
         @test (t1 - t2).x == (t1 + (-t2)).x
         @test (t1 + t2).x == (t1 - (-t2)).x
-    end    
+
+        @test t1 + t2 == t2 + t1
+        @test t1 - t2 == -(t2 - t1)
+        @test t1 - t1 == zero(t1)
+
+        @test 1 + d1 == d1 + 1
+        @test 1 - d1 == -(d1 - 1)
+
+        @test_throws DomainError 1 + t1
+        @test_throws DomainError 1 - t1
+        @test_throws DomainError t1 + 1
+        @test_throws DomainError t1 - 1
+    end
+
+    @testset "multiplication and division" begin
+        @test (t1 * f1).x == t1.x * f1.x
+        @test (t1 * f1).d == t1.d + f1.d
+
+        @test t1 * d1 == d1 * t1
+        @test t1 * d1 == d1.x * t1
+        @test t1 * d1 == t1 * d1.x
+
+        @test t1 + t1 == 2 * t1
+        @test t1 + 3.2 * t1 == 4.2 * t1
+
+        @test (t1 / f1).x == t1.x / f1.x
+        @test (t1 / f1).d == t1.d - f1.d
+
+        @test t1 / f1 ≈ t1 * (1 / f1)
+        @test t1 / t1 == 1
+
+        @test t1 + 1 / f1 ≈ (t1 * f1 + 1)
+    end
 end
