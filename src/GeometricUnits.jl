21dc4bfeec56fa7e408137c0c4afd948dfee6ad6
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-08-27 12:00:39 -0500

    GQ

--- src/GeometricUnits.jl
+++ src/GeometricUnits.jl
@@ -1,5 +1,18 @@
 module GeometricUnits
 
-greet() = print("Hello World!")
+import Base.+
+
+dimension_mismatch(obj) = throw(DomainError(obj, "Dimension mismatch!")) 
+
+struct GQ{X <: Real}
+    x::X
+    d::Int
+end 
+
+unit(a::GQ) = typeof(a)(1, a.d)
+
+zero(a::GQ) = typeof(a)(0, a.d)
+
+a::GQ + b::GQ = (a.d == b.d) ? typeof(a)(a.x+b.x, a.d) : dimension_mismatch(obj)
 
 end # module GeometricUnits
