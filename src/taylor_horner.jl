880c6a598b94ecb0fb2cbdd1029ff442199afb87
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-09-12 15:36:31 -0500

    format

--- src/taylor_horner.jl
+++ src/taylor_horner.jl
@@ -14,12 +14,16 @@ function TaylorSeries(t0::GQ{X}, c0::GQ{X}, cs::Vector{GQ{X}}) where {X<:Abstrac
     return TaylorSeries(cs_rev, c0, t0)
 end
 
-function _validate_taylor_series(t0::GQ{X}, c0::GQ{X}, cs::Vector{GQ{X}}) where {X<:AbstractFloat}
+function _validate_taylor_series(
+    t0::GQ{X},
+    c0::GQ{X},
+    cs::Vector{GQ{X}},
+) where {X<:AbstractFloat}
     dcs = [c.d for c in cs]
     nc = length(cs)
     dc0 = c0.d
     dt0 = t0.d
-    dcs1 = dc0 .- (1:nc).*dt0
+    dcs1 = dc0 .- (1:nc) .* dt0
     if dcs != dcs1
         dimension_mismatch((cs, c0, t0))
     end
@@ -32,10 +36,10 @@ function (th::TaylorSeries)(t::GQ)
     t_t0 = (t - th.t0).x
     n = length(th)
     for (ii, c) in enumerate(th.cs_rev)
-        result = result*t_t0/(n-ii+1) + c
+        result = result * t_t0 / (n - ii + 1) + c
         # print(result)
     end
-    return quantity_like(th.c0, th.c0.x + t_t0*result)
+    return quantity_like(th.c0, th.c0.x + t_t0 * result)
 end
 
 # a0 + x*(a1 + x*(a2 + a3*x))
